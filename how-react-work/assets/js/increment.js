0c55ec2f1e4daeecd4af6910b37a51db002f91f2
Tusar <tusarahmed141@gmail.com>
2022-07-15 15:57:46 +0600

    useState explore

--- how-react-work/assets/js/increment.js
+++ how-react-work/assets/js/increment.js
@@ -1,12 +1,14 @@
 const domContainer = document.querySelector("#root");
 
 const Increment = () => {
+  const [count, setCount] = React.useState(0);
+  const incC = () => setCount(count + 1);
   return (
     <div className="container">
       <div className="increment-container">
-        <h1 id="count">0</h1>
+        <h1 id="count">{count}</h1>
         <div>
-          <button id="increased">Increment+</button>
+          <button onClick={incC} id="increased">Increment+</button>
         </div>
       </div>
     </div>
@@ -14,18 +16,22 @@ const Increment = () => {
 };
 
 
-ReactDOM.render(<Increment />, domContainer);
+ReactDOM.render(<>
+  <Increment></Increment>
+  <Increment></Increment>
+  <Increment></Increment>
+</>, domContainer);
 
-let count = 0;
+// let count = 0;
 
-const increasedBtn = document.getElementById('increased');
-const countValue = document.getElementById('count');
+// const increasedBtn = document.getElementById('increased');
+// const countValue = document.getElementById('count');
 
-increasedBtn.addEventListener('click', e => {
-  count++;
-  console.log(count);
-  countValue.textContent = count;
-})
+// increasedBtn.addEventListener('click', e => {
+//   count++;
+//   console.log(count);
+//   countValue.textContent = count;
+// })
 
 
 
