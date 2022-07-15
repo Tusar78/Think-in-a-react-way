6585ac4f829284db435aee085d649a8511a5623d
Tusar <tusarahmed141@gmail.com>
2022-07-15 16:26:45 +0600

    Added batch update this is maximum optimized to dom

---
+++ how-react-work/assets/js/dom.js
@@ -0,0 +1,11 @@
+// Batch update more than faster
+let arr = [];
+let increment = 0;
+const container = document.querySelector('.container');
+
+while (increment < 10000) {
+  arr.push(++increment);
+}
+
+container.innerText = arr.join(' ');
+
