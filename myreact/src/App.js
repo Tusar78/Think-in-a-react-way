05dce1ac3a86e0d5d44beba295f211c96f46a98a
Tusar <tusarahmed141@gmail.com>
2022-07-19 18:59:02 +0600

    Add simple element

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,9 +1,9 @@
-function App() {
-  return (
-   <>
-    <p>Hello React!</p>
-   </>
-  );
-}
+import React from "react";
+
+const App = () => {
+  const name = "Tusar";
+  const element = <h1>Hello, {name}</h1>;
+  return <div>{element}</div>;
+};
 
 export default App;
