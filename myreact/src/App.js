6cef64a9ad363629b32510a1406edb8f730d0ea8
Tusar <tusarahmed141@gmail.com>
2022-07-19 23:13:43 +0600

    Try make reuseable component

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,23 +1,64 @@
 import React from "react";
 
-const formatUser = (user) => {
-  return `${user.firstName} ${user.lastName}`;
-};
+const formatDate = dt => {
+  return dt.toLocaleDateString();
+}
 
-const user = {
-  firstName: "Tusar",
-  lastName: "Ali",
-};
-
-const getGreeting = (user) => {
-  if (user) {
-    return <h1>Hello, {formatUser(user)}</h1>;
+class Avatar extends React.Component {
+  render() {
+    const {name, avatarURL} = this.props.author;
+    return (
+      <div className="avatar">
+        <img src={avatarURL} alt={name} />
+      </div>
+    )
   }
-  return <h1>Hello, Stranger.</h1>;
-};
+}
+
+class UserInfo extends React.Component {
+  render() {
+    const {author, author: {name}} = this.props.comment;
+    return (
+      <div className="user__info">
+        <Avatar author={author}></Avatar>
+        <div className="user__info-name">
+          <h2>{name}</h2>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Comment extends React.Component {
+  render() {
+    const {text, date} = this.props.comment;
+    return (
+      <div className="user__name">
+        <UserInfo comment={this.props.comment}></UserInfo>
+        <div className="comment__text">
+          <p>{text}</p>
+        </div>
+        <div className="comment__date">
+          {formatDate(date)}
+        </div>
+      </div>
+    )
+  }
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kittu',
+    avatarURL: 'http://placekitten.com/g/64/64'
+  }
+}
 
 const App = () => {
-  return <div>{getGreeting(user)}</div>;
+  return (
+    <Comment comment={comment}></Comment>
+  )
 };
 
 export default App;
