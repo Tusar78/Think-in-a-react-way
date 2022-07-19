38e2bdc3eab84250f282c76b875b572d83a37f0c
Tusar <tusarahmed141@gmail.com>
2022-07-19 20:24:57 +0600

    Syntax issue

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
 
-const formatUser = user => {
-  return `${user.firstName} ${user.lastName}`
-}
+const formatUser = (user) => {
+  return `${user.firstName} ${user.lastName}`;
+};
 
 const user = {
-  firstName: 'Tusar',
-  lastName: 'Ali'
-}
+  firstName: "Tusar",
+  lastName: "Ali",
+};
 
-const getGreeting = user => {
+const getGreeting = (user) => {
   if (user) {
-    return <h1>Hello, {formatUser(user)}</h1>
+    return <h1>Hello, {formatUser(user)}</h1>;
   }
-  return <h1>Hello, Stranger.</h1>
-}
+  return <h1>Hello, Stranger.</h1>;
+};
 
-const App = () => {  
-  return (
-    <div>{getGreeting(user)}</div>
-  );
+const App = () => {
+  return <div>{getGreeting(user)}</div>;
 };
 
 export default App;
