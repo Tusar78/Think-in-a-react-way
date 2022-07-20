c49397a3e079a83aae7e6db5731f29ac558341c2
Tusar <tusarahmed141@gmail.com>
2022-07-20 11:27:25 +0600

    Explore componentDidMount method

--- myreact/src/Components/Clock/Clock.js
+++ myreact/src/Components/Clock/Clock.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        date: new Date()
+      })
+    }, 1000);
+  }
+
   render() {
     return (
       <div>
         <h2>Hello React!</h2>
-        <p>It's {new Date().toLocaleTimeString(this.props.locale)}</p>
+        <p>It's {this.state.date.toLocaleTimeString(this.props.locale)}</p>
       </div>
     );
   }
