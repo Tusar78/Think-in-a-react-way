90931519b5977a068737e5cf69b8806f0eec763a
Tusar <tusarahmed141@gmail.com>
2022-07-25 19:05:01 +0600

    Controled form

--- myreact/src/Components/Form/Form.js
+++ myreact/src/Components/Form/Form.js
@@ -6,6 +6,8 @@ class Form extends Component {
     this.state = {
       value: "",
       message: "",
+      option: '',
+      term: false
     };
   }
 
@@ -23,19 +25,37 @@ class Form extends Component {
           message: e.target.value,
         };
       });
+    } else if (type === 'select-one') {
+      this.setState((prevState, props) => {
+        return {
+          option: e.target.value,
+        };
+      });
+    } else if (type === 'checkbox') {
+      this.setState((prevState, props) => {
+        return {
+          term: e.target.checked,
+        };
+      });
     } else {
       console.log('Nothing type');
     }
   };
+
+  submitChange = e => {
+    const { value, message, term, option } = this.state;
+    e.preventDefault();
+    console.log(value, message, term, option);
+  }
   render() {
-    const { value, message } = this.state;
+    const { value, message, option, term } = this.state;
     let display =
       value && value.trim().length > 0 ? (
         <p className="display__input">{value}</p>
       ) : null;
 
     return (
-      <form>
+      <form onSubmit={this.submitChange}>
         <input
           type="text"
           name="username"
@@ -44,15 +64,24 @@ class Form extends Component {
           value={value}
           onChange={this.handleChange}
         />
-
+        <select value={option} onChange={this.handleChange}>
+          <option value="React">React</option>
+          <option value="Angular">Angular</option>
+        </select>
         <textarea
           name="message"
           placeholder="Type message"
           value={message}
           onChange={this.handleChange}
-        ></textarea>
+        ></textarea>       
 
+        <div className="terms__condition">
+          <input type="checkbox" id="term" checked={term} onChange={this.handleChange}/>  
+          <label htmlFor="term">Term's and condition</label>
+        </div> 
         {display}
+
+        <button type="submit">Submit</button>
       </form>
     );
   }
