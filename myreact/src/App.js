b41a1f7d0e1dd4274314fd849ebf104732f717f7
Tusar <tusarahmed141@gmail.com>
2022-07-28 11:19:22 +0600

    Prepare new component like boiling verdict

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import './App.css'
-import Form from "./ComponentsTwo/Form/Form";
 
 const App = () => {
   return (
     <div className="container">
-      <Form />
+
     </div>
   );
 };
