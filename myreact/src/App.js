b5919092a0547de3f18c22b9caa388f1ae7cde09
Tusar <tusarahmed141@gmail.com>
2022-07-28 12:42:48 +0600

    Prepare to inheritance

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import './App.css'
-import Calculator from "./ComponentsTwo/Boiling/Calculator";
 
 const App = () => {
   return (
     <div className="container">
-      <Calculator />
     </div>
   );
 };
