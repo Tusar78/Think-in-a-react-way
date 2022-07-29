8e4fc1a9e8ad85950d8c0566b3079366bbadda3d
Tusar <tusarahmed141@gmail.com>
2022-07-29 17:36:27 +0600

    Add product youtube style

--- myreact/src/utilities/fakedb.js
+++ myreact/src/utilities/fakedb.js
@@ -1,13 +1,27 @@
-const fakeDB = (id) => {
-  const quantity = localStorage.getItem(id);
-  if (quantity) {
-    console.log('already exist');
-    const newQuantity = parseFloat(quantity) + 1;
-    localStorage.setItem(id, newQuantity)
-  } else {
-    console.log('Added new item.');
-    localStorage.setItem(id, 1);
-  }
-}
+// const fakeDB = (id) => {
+//   const quantity = localStorage.getItem(id);
+//   if (quantity) {
+//     console.log('already exist');
+//     const newQuantity = parseFloat(quantity) + 1;
+//     localStorage.setItem(id, newQuantity)
+//   } else {
+//     console.log('Added new item.');
+//     localStorage.setItem(id, 1);
+//   }
+// }
 
-export {fakeDB}
+const fakeDB = (id) => {
+  let shoppingCart = {};
+
+  const quantity = shoppingCart[id];
+  console.log(quantity);
+  if (quantity) {
+    const newQuantity = quantity + 1;
+    shoppingCart[id] = newQuantity;
+  } else {
+    shoppingCart[id] = 1;
+  }
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+};
+
+export { fakeDB };
