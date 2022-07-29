e50e0533546a654cecdba82431d9f5be57ede9d9
Tusar <tusarahmed141@gmail.com>
2022-07-29 11:56:30 +0600

    Add class name of sigle cosmetic

--- myreact/src/ComponentsThree/Cosmetic/Cosmetic.js
+++ myreact/src/ComponentsThree/Cosmetic/Cosmetic.js
@@ -3,7 +3,7 @@ import React from "react";
 const Cosmetic = ({ cosmetic }) => {
   const { id, name, price } = cosmetic;
   return (
-    <div>
+    <div className="display__input">
       <h3>Product name: {name}</h3>
       <p>Price: {price}</p>
       <small>It's id: {id}</small>
