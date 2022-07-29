a9c97c169526098fa517c73787febe12cd58c406
Tusar <tusarahmed141@gmail.com>
2022-07-29 20:49:23 +0600

    Practice reduced

--- myreact/src/utilities/calculate.js
+++ myreact/src/utilities/calculate.js
@@ -1,9 +1,15 @@
 const add = (num1, num2) => {
   return num1 + num2;
-}
+};
 
 const multiply = (num1, num2) => {
   return num1 * num2;
-}
+};
 
-export {add, multiply}
+const numbers = [5, 7, 8, 6, 9, 4];
+const reducedArray = numbers.reduce((previousNumber, currentNumber) => {
+  return previousNumber + currentNumber;
+}, 0);
+console.log(reducedArray);
+
+export { add, multiply, reducedArray };
