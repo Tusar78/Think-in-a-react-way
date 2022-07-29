652875a2c272913ae816ba4f3026aebf0e6fdcee
Tusar <tusarahmed141@gmail.com>
2022-07-29 15:49:23 +0600

    explore local storage

--- myreact/src/utilities/fakedb.js
+++ myreact/src/utilities/fakedb.js
@@ -2,6 +2,8 @@ const fakeDB = (id) => {
   const quantity = localStorage.getItem(id);
   if (quantity) {
     console.log('already exist');
+    const newQuantity = parseFloat(quantity) + 1;
+    localStorage.setItem(id, newQuantity)
   } else {
     console.log('Added new item.');
     localStorage.setItem(id, 1);
