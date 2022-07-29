88ed30b9e211b5985731cfdc1a64258b29cb47e5
Tusar <tusarahmed141@gmail.com>
2022-07-29 14:21:10 +0600

    Explore event handler

--- myreact/src/ComponentsThree/Cosmetic/Cosmetic.js
+++ myreact/src/ComponentsThree/Cosmetic/Cosmetic.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Cosmetic = ({ cosmetic }) => {
   const { _id, name, balance } = cosmetic;
+  const adToCart = (balance) => {
+    console.log(`This product Balance: ${balance}`);
+  }
   return (
     <div className="display__input">
       <h3>Product name: {name}</h3>
       <p>Price: {balance}</p>
       <small>It's id: {_id}</small>
+      <br />
+      <button onClick={() => adToCart(balance)}>Ad to cart</button>
     </div>
   );
 };
