fa5534609d0a2109e0e82ab0c356e30ca76e7d00
Tusar <tusarahmed141@gmail.com>
2022-08-02 21:51:07 +0600

    Pass annonymous function to child component via props

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -1,11 +1,13 @@
 import Click from "./Components/Inner/Click/Click";
 import Hover from "./Components/Inner/Hover/Hover";
+import User from "./Components/Inner/User/User";
 
 function App() {
   return (
     <>
       <Click />
       <Hover />
+      <User name={(isLoggedIn) => isLoggedIn ? 'Tusar' : 'Foinni'}/>
     </>
   );
 }
---
+++ simple-react/src/Components/Inner/User/User.js
@@ -0,0 +1,5 @@
+const User = ({name}) => {
+  return name(true)
+};
+
+export default User;
