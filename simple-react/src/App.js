a6f6df79171d5f8d5a1e41b664a96285b9000155
Tusar <tusarahmed141@gmail.com>
2022-08-02 18:56:17 +0600

    Prepare to clear HOC component

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -1,15 +1,7 @@
-import './App.css';
-import Emoji from './Component/Inheritance/Emoji';
-import Text from './Component/Testing/Text';
-
 function App() {
   return (
     <>
-      <Emoji>
-        {
-          ({addEmoji}) => <Text adEmoji={addEmoji} />
-        }
-      </Emoji>
+      
     </>
   );
 }
